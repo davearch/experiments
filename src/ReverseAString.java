6d6b320175f323d0bce9d5f7a9a931ded443e3f5
davearch <davearch@email.arizona.edu>
2013-11-10 23:34:24 -0700

    first problem

---
+++ src/ReverseAString.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ReverseAString{
+	public static void main(String args[]){
+		
+		// enter a string and the program will reverse it and print it out.
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter any string that you want:");
+		String stringToReverse = input.nextLine();
+		
+		int length = stringToReverse.length();
+		char[] oldString = new char[length];
+		char[] newString = new char[length];
+		
+		// put string into char array
+		for (int i = 0; i < length; i++) {
+			oldString[i] = stringToReverse.charAt(i);
+		}
+		
+		// reverse string
+		for (int i = 0; i < length; i++){
+			newString[i] = oldString[length - 1 -i];
+		}
+		
+		// print string
+		System.out.println(newString);
+	}
+}
