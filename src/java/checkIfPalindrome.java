4ed0af5da534d1ec1d884b875100be719e749855
davearch <davearch@email.arizona.edu>
2014-01-28 22:15:05 -0700

    changed directory to include python

---
+++ src/java/checkIfPalindrome.java
@@ -0,0 +1,13 @@
+package experiments.src;
+import java.util.*;
+
+public class checkIfPalindrome {
+    public static void main (String[] args) {
+	// enter a word and the program will see if it is a palindrome
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter word to check if it is a palindrome: ");
+	String wordToCheck = input.next();
+
+	
+    }
+}
