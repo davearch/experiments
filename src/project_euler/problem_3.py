5d3fe86df3bb37f5409ac2085de38dd0d931f35c
davearch <davearch@email.arizona.edu>
2015-01-15 11:57:24 -0700

    webScrap and project ueler

---
+++ src/project_euler/problem_3.py
@@ -0,0 +1,14 @@
+# Project Euler Problem 3
+
+def prob():
+    num = 600851475143
+    while True:
+        fact = getFact(num)
+        if fact < num:
+            num != fact
+        else:
+            return num
+
+
+def getFact(number):
+    for 
